2e58dcacecd83317017ce8237f2d4995d3d190b7
maiconper <maicon.code.pereira@gmail.com>
2025-06-05 17:35:17 -0300

    primeiro commit

---
+++ src/main/java/com/example/antisocial/AntisocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.antisocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntisocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntisocialApplication.class, args);
+	}
+
+}
